4d5d4953cc475bbee39b18ad94e78c0e248d62c2
steve desire <stevedesiredev@gmail.com>
2023-08-19 03:38:30 +0200

    first commit. Project implemening a micro service in java and using rest connector and graphql connector as web service. This project help to develop understanding and implementation of rest and graphql web service

---
+++ src/main/java/steve/dev/steve_bankaccountservice_ws/SteveBankAccountServiceWsApplication.java
@@ -0,0 +1,13 @@
+package steve.dev.steve_bankaccountservice_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteveBankAccountServiceWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteveBankAccountServiceWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/steve/dev/steve_bankaccountservice_ws/SteveBankAccountServiceWsApplicationTests.java
@@ -0,0 +1,13 @@
+package steve.dev.steve_bankaccountservice_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteveBankAccountServiceWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
