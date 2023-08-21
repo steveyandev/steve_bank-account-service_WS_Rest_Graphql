65444ab16ff4bc565fd4a21197700c0b9e5b2021
steve desire <stevedesiredev@gmail.com>
2023-08-22 01:57:05 +0200

    adding swagger deppendency for documentation of our API. for recent version of swagger 2 and 3 for springBoot new version just ading dependency will not work need to create a config class for swagger in our project.

--- src/main/java/steve/dev/steve_bankaccountservice_ws/entities/BankAccount.java
+++ src/main/java/steve/dev/steve_bankaccountservice_ws/entities/BankAccount.java
@@ -1,16 +1,18 @@
 package steve.dev.steve_bankaccountservice_ws.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import steve.dev.steve_bankaccountservice_ws.enums.AccountType;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
 import java.util.Date;
+
 
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
