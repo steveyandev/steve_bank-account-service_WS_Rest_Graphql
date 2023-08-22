6081fedb93c74008abd5ea1c026c5e317c7abf97
steve desire <stevedesiredev@gmail.com>
2023-08-22 03:17:02 +0200

    adding the method findByType in our repository this possible by Spring Data rest and using it to get BankAccounts by type by using the search option as http://localhost:8081/bankAccounts/search/findByType?type=SAVING_ACCOUNT to get just SAVING BANK ACCOUNTS

--- src/main/java/steve/dev/steve_bankaccountservice_ws/repositories/BankAccountRepository.java
+++ src/main/java/steve/dev/steve_bankaccountservice_ws/repositories/BankAccountRepository.java
@@ -3,7 +3,12 @@ package steve.dev.steve_bankaccountservice_ws.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import steve.dev.steve_bankaccountservice_ws.entities.BankAccount;
+import steve.dev.steve_bankaccountservice_ws.enums.AccountType;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
+
+    List<BankAccount> findByType(AccountType type);
 }
