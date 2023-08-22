4bd7f402d7d7231e9b120d14591312af8bc30395
steve desire <stevedesiredev@gmail.com>
2023-08-22 02:59:00 +0200

    ading spring data rest dependency and use it genric interface to implement crud operation on bankAccount entity. Nowwe have 2 rest api in our project. Spring Data rest rest come with in build method for crud operation and made simple by just using the @RepositoryRestRessource in our Repository class

--- src/main/java/steve/dev/steve_bankaccountservice_ws/repositories/BankAccountRepository.java
+++ src/main/java/steve/dev/steve_bankaccountservice_ws/repositories/BankAccountRepository.java
@@ -1,7 +1,9 @@
 package steve.dev.steve_bankaccountservice_ws.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import steve.dev.steve_bankaccountservice_ws.entities.BankAccount;
 
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
 }
--- src/main/java/steve/dev/steve_bankaccountservice_ws/web/AccountRestRepository.java
+++ src/main/java/steve/dev/steve_bankaccountservice_ws/web/AccountRestRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api")
 public class AccountRestRepository {
     private BankAccountRepository bankAccountRepository;
 
