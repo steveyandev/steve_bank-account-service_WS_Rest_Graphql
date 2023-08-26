a2db8178caf3c9362ec78e6272d0b06d418bc8ff
steve desire <stevedesiredev@gmail.com>
2023-08-26 22:17:06 +0200

    implmentation of the class CustomDataFetcherExceptionResolver as handler for exception to get the messae when exception arrive for example loking for an acount that doen't exist

---
+++ src/main/java/steve/dev/steve_bankaccountservice_ws/exceptions/CustomDataFetcherExceptionResolver.java
@@ -0,0 +1,31 @@
+package steve.dev.steve_bankaccountservice_ws.exceptions;
+
+import graphql.ErrorClassification;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+
+import java.util.List;
+
+public class CustomDataFetcherExceptionResolver extends DataFetcherExceptionResolverAdapter {
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        return new GraphQLError() {
+            @Override
+            public String getMessage() {
+                return ex.getMessage();
+            }
+
+            @Override
+            public List<SourceLocation> getLocations() {
+                return null;
+            }
+
+            @Override
+            public ErrorClassification getErrorType() {
+                return null;
+            }
+        } ;
+    }
+}
