45b5aca776431ccadd3ee3e6f1c323366ebf884b
steve desire <stevedesiredev@gmail.com>
2023-09-20 14:26:37 +0200

    implementing mutation method to handle post reqest for adding a new bank account

--- src/main/java/steve/dev/steve_bankaccountservice_ws/exceptions/CustomDataFetcherExceptionResolver.java
+++ src/main/java/steve/dev/steve_bankaccountservice_ws/exceptions/CustomDataFetcherExceptionResolver.java
@@ -5,9 +5,11 @@ import graphql.GraphQLError;
 import graphql.language.SourceLocation;
 import graphql.schema.DataFetchingEnvironment;
 import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class CustomDataFetcherExceptionResolver extends DataFetcherExceptionResolverAdapter {
     @Override
     protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
--- src/main/java/steve/dev/steve_bankaccountservice_ws/web/BankAccountGraphQLController.java
+++ src/main/java/steve/dev/steve_bankaccountservice_ws/web/BankAccountGraphQLController.java
@@ -1,11 +1,18 @@
 package steve.dev.steve_bankaccountservice_ws.web;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import steve.dev.steve_bankaccountservice_ws.dto.BankAccountRequestDTO;
+import steve.dev.steve_bankaccountservice_ws.dto.BankAccountResponseDTO;
 import steve.dev.steve_bankaccountservice_ws.entities.BankAccount;
 import steve.dev.steve_bankaccountservice_ws.repositories.BankAccountRepository;
+import steve.dev.steve_bankaccountservice_ws.service.AccountService;
 
 import java.util.List;
 
@@ -13,7 +20,8 @@ import java.util.List;
 public class BankAccountGraphQLController {
     @Autowired
     private BankAccountRepository bankAccountRepository;
-
+    @Autowired
+    private AccountService accountService;
     @QueryMapping
     public List<BankAccount> accountsList(){
         return bankAccountRepository.findAll();
@@ -23,4 +31,20 @@ public class BankAccountGraphQLController {
         return bankAccountRepository.findById(id)
                 .orElseThrow(()->new RuntimeException(String.format("Account %s not found",id)));
     }
+    @MutationMapping
+    public BankAccountResponseDTO addAccount(@Argument BankAccountRequestDTO bankAccount){
+       return accountService.addAccount(bankAccount);
+    }
 }
+/*@Data @NoArgsConstructor @AllArgsConstructor
+class BankAccountDTO{
+    private String type;
+    private  Double balance;
+    private String currency;
+}*/
+/*
+record BankAccountDTO(Double balance, String type,String currency ){
+
+}
+*/
+
