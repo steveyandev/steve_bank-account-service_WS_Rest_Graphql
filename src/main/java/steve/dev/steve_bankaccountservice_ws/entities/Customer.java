54672bfd96e1f7e19734ea1aef43ba50613dc003
steve desire <stevedesiredev@gmail.com>
2023-09-29 00:35:31 +0200

    add of JsonProperty on bankAccounts attribute of customer in Access WRITE_ONLY to avoid infinite loop when loading data in json format using of rest api

--- src/main/java/steve/dev/steve_bankaccountservice_ws/entities/Customer.java
+++ src/main/java/steve/dev/steve_bankaccountservice_ws/entities/Customer.java
@@ -1,5 +1,6 @@
 package steve.dev.steve_bankaccountservice_ws.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,5 +16,6 @@ public class Customer {
     private Long id;
     private String name;
     @OneToMany(mappedBy ="customer")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<BankAccount> bankAccounts;
 }
